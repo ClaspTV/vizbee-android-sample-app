0f9ac587246eab90e41247948e70864be322c257
sreenu cheedella <sreenu@vizbee.tv>
2025-08-13 14:12:17 +0530

    Updated the styling with reference to NFL

--- app/src/main/java/tv/vizbee/demo/adapter/PlaylistAdapter.kt
+++ app/src/main/java/tv/vizbee/demo/adapter/PlaylistAdapter.kt
@@ -5,8 +5,6 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import tv.vizbee.demo.databinding.ItemPlaylistBinding
 import tv.vizbee.demo.model.VideoItem
 import tv.vizbee.demo.util.GlideUtil
